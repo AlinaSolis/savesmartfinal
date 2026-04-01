c34ae6c9f2fe62d9b7fc694936855984f5222f94
AlinaSolis <169116206+AlinaSolis@users.noreply.github.com>
2026-03-31 18:10:23 -0600

    🔥 Actualizacion

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,12 +1,48 @@
-// src/services/api.ts
+// src/services/authService.ts
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_AUTH_API;
+
+console.log("AUTH API:", API_URL); // debug
+
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // La URL de tu artisan serve
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
 });
+
+//LOGIN
+export const login = async (email: string, password: string) => {
+  const response = await api.post('/login', {
+    email,
+    password,
+  });
+
+  return response.data;
+};
+
+//Registro
+export const register = async (name: string, email: string, password: string) => {
+  const response = await api.post('/register', {
+    name,
+    email,
+    password,
+  });
+
+  return response.data;
+};
+
+// 👤 USER (opcional)
+export const getUser = async (token: string) => {
+  const response = await api.get('/user', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
 
 export default api;
